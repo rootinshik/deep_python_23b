1e8b7fe423ac57e7adf62f3769ae5ffc43bc7fbb
rootinshik <gurianevil@yandex.ru>
2023-09-18 12:25:27 +0300

    first commit

--- 01/predict_message_mood.py
+++ 01/predict_message_mood.py
@@ -7,4 +7,13 @@ def predict_message_mood(
     bad_thresholds: float = 0.3,
     good_thresholds: float = 0.8,
 ) -> str:
-    pass
+
+    model_prediction = model.predict(message)
+
+    if model_prediction < bad_thresholds:
+        return "неуд"
+    elif model_prediction < good_thresholds:
+        return "норм"
+    else:
+        return "отл"
+
