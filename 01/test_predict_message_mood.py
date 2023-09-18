81d8221852b3e4c7e594d75b69065e6c5314cbb4
rootinshik <gurianevil@yandex.ru>
2023-09-18 18:44:45 +0300

    first commit

--- 01/test_predict_message_mood.py
+++ 01/test_predict_message_mood.py
@@ -5,8 +5,32 @@ from predict_message_mood import predict_message_mood
 
 
 class TestPredictMessageMood(unittest.TestCase):
-    def test_normal_cond(self):
-        pass
+    @patch('predict_message_mood.SomeModel', autospec=True)
+    def test_normal_cond(self, mock_model):
+        mock_model = mock_model.return_value
+        mock_model.predict.side_effect = [0.2, 0.4, 0.9]
+
+        bad_thresholds = 0.3
+        good_thresholds = 0.8
+
+        result1 = predict_message_mood(message='Чапаев и Пустота',
+                                       model=mock_model,
+                                       bad_thresholds=bad_thresholds,
+                                       good_thresholds=good_thresholds)
+
+        result2 = predict_message_mood(message='Generation П',
+                                       model=mock_model,
+                                       bad_thresholds=bad_thresholds,
+                                       good_thresholds=good_thresholds)
+
+        result3 = predict_message_mood(message='Голубое сало',
+                                       model=mock_model,
+                                       bad_thresholds=bad_thresholds,
+                                       good_thresholds=good_thresholds)
+
+        self.assertEqual(result1, 'неуд')
+        self.assertEqual(result2, 'норм')
+        self.assertEqual(result3, 'отл')
 
     def test_edge_cond(self):
         pass
