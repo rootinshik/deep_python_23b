0f85518aaf1bffec4b3186385ccac36123bddfbd
rootinshik <gurianevil@yandex.ru>
2023-09-18 12:42:28 +0300

    first commit

--- 01/predict_message_mood.py
+++ 01/predict_message_mood.py
@@ -1,12 +1,26 @@
 from model import SomeModel
 
 
+class ThresholdsError(Exception):
+    def __init__(self,
+                 bad_thresholds: float,
+                 good_thresholds: float,
+                 message: str = "Bad threshold greater then good thresholds"
+                 ):
+        self.bad_thresholds = bad_thresholds
+        self.good_thresholds = good_thresholds
+        self.message = message
+        super().__init__(self.message + f': {self.bad_thresholds=}, {self.good_thresholds=}')
+
+
 def predict_message_mood(
-    message: str,
-    model: SomeModel,
-    bad_thresholds: float = 0.3,
-    good_thresholds: float = 0.8,
+        message: str,
+        model: SomeModel,
+        bad_thresholds: float = 0.3,
+        good_thresholds: float = 0.8,
 ) -> str:
+    if bad_thresholds > good_thresholds:
+        raise ThresholdsError(bad_thresholds, good_thresholds)
 
     model_prediction = model.predict(message)
 
@@ -16,4 +30,3 @@ def predict_message_mood(
         return "норм"
     else:
         return "отл"
-
