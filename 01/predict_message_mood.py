bfc9f8037e9d7e6846bd33197764869be668e6f2
rootinshik <gurianevil@yandex.ru>
2023-09-18 17:15:10 +0300

    first commit

--- 01/predict_message_mood.py
+++ 01/predict_message_mood.py
@@ -2,28 +2,28 @@ from model import SomeModel
 
 
 class ThresholdsError(Exception):
-    def __init__(self,
-                 bad_thresholds: float,
-                 good_thresholds: float,
-                 message: str = "Bad threshold greater then good threshold"
-                 ):
-        super().__init__(message + f': {bad_thresholds=}, {good_thresholds=}')
+    def __init__(
+        self,
+        bad_thresholds: float,
+        good_thresholds: float,
+        message: str = "Bad threshold greater then good threshold",
+    ):
+        super().__init__(message + f": {bad_thresholds=}, {good_thresholds=}")
 
 
 def predict_message_mood(
-        message: str,
-        model: SomeModel,
-        bad_thresholds: float = 0.3,
-        good_thresholds: float = 0.8,
+    message: str,
+    model: SomeModel,
+    bad_thresholds: float = 0.3,
+    good_thresholds: float = 0.8,
 ) -> str:
-
     if bad_thresholds > good_thresholds:
         raise ThresholdsError(bad_thresholds, good_thresholds)
 
     model_prediction = model.predict(message)
 
     if model_prediction < bad_thresholds:
-        return 'неуд'
+        return "неуд"
     if model_prediction > good_thresholds:
-        return 'отл'
-    return 'норм'
+        return "отл"
+    return "норм"
--- 01/test_predict_message_mood.py
+++ 01/test_predict_message_mood.py
@@ -1,4 +1,22 @@
 import unittest
-from unittest import mock
+from unittest.mock import patch
 
 from predict_message_mood import predict_message_mood
+
+
+class TestPredictMessageMood(unittest.TestCase):
+    def test_normal_cond(self):
+        pass
+
+    def test_edge_cond(self):
+        pass
+
+    def test_thresholds_equals(self):
+        pass
+
+    def test_swap_thresholds(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
