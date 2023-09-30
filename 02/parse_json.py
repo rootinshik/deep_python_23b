6551a2a5be7377d3b8dda5cd47de68975bb0fa46
rootinshik <gurianevil@yandex.ru>
2023-09-30 17:58:01 +0300

    Реализация первого задания

--- 02/parse_json.py
+++ 02/parse_json.py
@@ -1,5 +1,24 @@
 import json
 
 
-def parse_json(json_str: str, required_fields=None, keywords=None, keyword_callback=print):
-    ...
+def example_callback(key: str, value: str) -> None:
+    print(f"{key=}, {value=}")
+
+
+def parse_json(
+    json_str: str,
+    required_fields: list[str] = None,
+    keywords: list[str] = None,
+    keyword_callback: callable = None,
+) -> None:
+    json_doc = json.loads(json_str)
+    required_doc = {
+        required_fields[i]:
+            keywords.copy() for i in range(len(required_fields))
+    }
+
+    for key, values in json_doc.items():
+        if key in required_doc.keys():
+            for value in values.split():
+                if value in required_doc[key]:
+                    keyword_callback(key, value)
