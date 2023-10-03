539f37507173303302b724fc43af683c8c7e3e9d
rootinshik <gurianevil@yandex.ru>
2023-10-03 14:45:56 +0300

    Добавил правильную типизацию

--- 01/find_in_file.py
+++ 01/find_in_file.py
@@ -3,15 +3,20 @@ from io import IOBase
 
 
 def find_in_file(
-    file: Union[str, TextIO], words_to_find: list[str], encoding: str = "UTF-8"
+        file: Union[str, TextIO],
+        words_to_find: list[str],
+        encoding: str = "UTF-8"
 ) -> Generator[str, None, None]:
+
     def words_in_line(search_line: bytes, words: list[str]) -> bool:
         for word in map(lambda str_: str_.lower(), words):
             if word in search_line.lower().split():
                 return True
         return False
 
-    def base_generator(file_, words_to_find_):
+    def base_generator(file_: Union[TextIO, IOBase, bytes, str, int],
+                       words_to_find_: list[str]
+                       ) -> Generator[str, None, None]:
         for line in file_:
             if words_in_line(line, words_to_find_):
                 yield line.strip()
