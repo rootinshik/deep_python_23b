25aa443b1c2240d82ed4f1dcaaed9253a6ae33c4
rootinshik <gurianevil@yandex.ru>
2023-10-15 00:15:12 +0300

    Реализация + тесты + отчет

--- 04/descriptors.py
+++ 04/descriptors.py
@@ -21,6 +21,41 @@ class Probability:
         return setattr(obj, self.name, val)
 
 
+class LatexInLineMathEquation:
+    def __set_name__(self, owner, name):
+        self.name = f"_LatexInLineMathEquation_descr_{name}"
+
+    def __get__(self, obj, obj_type):
+        if obj is None:
+            return None
+
+        return getattr(obj, self.name)
+
+    def __set__(self, obj, val):
+        if obj is None:
+            return None
+
+        if len(val) <= 2 or val[0] != "$" \
+                or val[-1] != "$" \
+                or val.count("$") > 2:
+            raise ValueError("Incorrect inline math equation")
+
+        if not LatexInLineMathEquation.is_brackets_closed(val, {"{": "}"}):
+            raise ValueError("{} brackets not closed")
+
+        return setattr(obj, self.name, val)
+
+    @staticmethod
+    def is_brackets_closed(string, brackets):
+        brackets_queue = []
+        for symb in string:
+            if symb in brackets:
+                brackets_queue.append(brackets[symb])
+            elif symb in brackets_queue:
+                brackets_queue.pop()
+        return len(brackets_queue) == 0
+
+
 class FootballMatchScore:
     def __set_name__(self, owner, name):
         self.name = f"_FootballMatchScore_desc_{name}"
@@ -47,25 +82,3 @@ class FootballMatchScore:
             raise ValueError("Scores must be natural")
 
         return setattr(obj, self.name, list(scores))
-
-class Probability:
-    def __set_name__(self, owner, name):
-        self.name = f"_prob_descr_{name}"
-
-    def __get__(self, obj, obj_type):
-        if obj is None:
-            return None
-
-        return getattr(obj, self.name)
-
-    def __set__(self, obj, val):
-        if obj is None:
-            return None
-
-        if not isinstance(val, (float, int)):
-            raise ValueError("Int or float required")
-
-        if not 0 <= val <= 1:
-            raise ValueError("Probability must be in [0; 1]")
-
-        return setattr(obj, self.name, val)
--- 04/descriptors_test.py
+++ 04/descriptors_test.py
@@ -1,44 +1,166 @@
 import unittest
 
-from descriptors import Probability, FootballMatchScore
-
-import unittest
+from descriptors import Probability
+from descriptors import FootballMatchScore
+from descriptors import LatexInLineMathEquation
 
 
 class TestProbabilityDescriptor(unittest.TestCase):
-    def test_valid_probability(self):
-        class Event:
+    def test_probability_descriptor_valid(self):
+        class SomeClass:
             probability = Probability()
 
-        event = Event()
-        event.probability = 0.5
-        self.assertEqual(event.probability, 0.5)
+        obj = SomeClass()
+        obj.probability = 0.5
+        self.assertEqual(obj.probability, 0.5)
+        obj.probability = 0.3
+        self.assertEqual(obj.probability, 0.3)
 
-    def test_invalid_probability(self):
-        class Event:
+    def test_probability_descriptor_edge_cond(self):
+        class SomeClass:
             probability = Probability()
 
-        event = Event()
+        obj = SomeClass()
+        obj.probability = 0
+        self.assertEqual(obj.probability, 0)
+        obj.probability = 1
+        self.assertEqual(obj.probability, 1)
+
+    def test_probability_descriptor_invalid_type(self):
+        class SomeClass:
+            probability = Probability()
+
+        obj = SomeClass()
         with self.assertRaises(ValueError):
-            event.probability = 1.5
+            obj.probability = "Вчера сходил на разводные мосты"
+
+    def test_probability_descriptor_invalid_range(self):
+        class SomeClass:
+            probability = Probability()
+
+        obj = SomeClass()
+
+        with self.assertRaises(ValueError):
+            obj.probability = 1.2
+
+        with self.assertRaises(ValueError):
+            obj.probability = -0.1
+
+    def test_probability_descriptor_is_none(self):
+        class SomeClass:
+            probability = Probability()
+
+        self.assertEqual(SomeClass.probability, None)
+
+
+class TestLatexInLineMathEquationDescriptor(unittest.TestCase):
+    def test_latex_descriptor_valid(self):
+        class SomeClass:
+            latex = LatexInLineMathEquation()
+
+        obj = SomeClass()
+        obj.latex = "$a + b$"
+        self.assertEqual(obj.latex, "$a + b$")
+
+    def test_latex_descriptor_invalid_format(self):
+        class SomeClass:
+            latex = LatexInLineMathEquation()
+
+        obj = SomeClass()
+        with self.assertRaises(ValueError):
+            obj.latex = "a + b"
+
+        with self.assertRaises(ValueError):
+            obj.latex = "$a + b"
+
+        with self.assertRaises(ValueError):
+            obj.latex = "a + b$"
+
+        for num in range(10):
+            with self.assertRaises(ValueError):
+                obj.latex = "$" * num
+
+    def test_latex_descriptor_unclosed_brackets(self):
+        class SomeClass:
+            latex = LatexInLineMathEquation()
+
+        obj = SomeClass()
+        with self.assertRaises(ValueError):
+            obj.latex = r"$\frac{a + b$"
+
+    def test_latex_descriptor_is_none(self):
+        class MyClass:
+            latex = LatexInLineMathEquation()
+
+        self.assertEqual(MyClass.latex, None)
 
 
 class TestFootballMatchScoreDescriptor(unittest.TestCase):
-    def test_valid_scores(self):
-        class Match:
+    def test_score_descriptor_valid_list(self):
+        class SomeClass:
             score = FootballMatchScore()
 
-        match = Match()
-        match.score = [2, 1]
-        self.assertEqual(match.score, [2, 1])
+        obj = SomeClass()
+        obj.score = [2, 1]
+        self.assertEqual(obj.score, [2, 1])
+        obj.score = [0, 0]
+        self.assertEqual(obj.score, [0, 0])
+        obj.score = [1, 2]
+        self.assertEqual(obj.score, [1, 2])
 
-    def test_invalid_scores(self):
-        class Match:
+        obj.score = (2, 1)
+        self.assertEqual(obj.score, [2, 1])
+        obj.score = (0, 0)
+        self.assertEqual(obj.score, [0, 0])
+        obj.score = (1, 2)
+
+        self.assertEqual(obj.score, [1, 2])
+        obj.score = {"Зенит": 7, "Спартак": 1}
+        self.assertEqual(obj.score, [7, 1])
+        obj.score = {"Сокол (Саратов)": 1, "Химки": 0}
+        self.assertEqual(obj.score, [1, 0])
+        obj.score = {"Динамо": 2, "ЦСКА": 1}
+        self.assertEqual(obj.score, [2, 1])
+
+    def test_score_descriptor_invalid_type(self):
+        class MyClass:
             score = FootballMatchScore()
 
-        match = Match()
+        obj = MyClass()
         with self.assertRaises(ValueError):
-            match.score = [2, "1"]
+            obj.score = "Зенит обыграл спартак со счетом 7:1"
+
+    def test_score_descriptor_invalid_length(self):
+        class MyClass:
+            score = FootballMatchScore()
+
+        obj = MyClass()
+        with self.assertRaises(ValueError):
+            obj.score = [2]
+        obj = MyClass()
+        with self.assertRaises(ValueError):
+            obj.score = {"Зенит": 0}
+        obj = MyClass()
+        with self.assertRaises(ValueError):
+            obj.score = (1,)
+        with self.assertRaises(ValueError):
+            obj.score = [2, 4, 1]
+
+    def test_score_descriptor_invalid_score_values(self):
+        class MyClass:
+            score = FootballMatchScore()
+
+        obj = MyClass()
+        with self.assertRaises(ValueError):
+            obj.score = [2, -1]
+        with self.assertRaises(ValueError):
+            obj.score = [2.1, 2.19]
+
+    def test_score_descriptor_is_none(self):
+        class MyClass:
+            score = FootballMatchScore()
+
+        self.assertEqual(MyClass.score, None)
 
 
 if __name__ == "__main__":
