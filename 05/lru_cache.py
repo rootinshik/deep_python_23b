8310caeefa9fdfe4b55cda7d5c5a9a421d34925f
rootinshik <gurianevil@yandex.ru>
2023-10-25 19:14:01 +0300

    Небольшие правки

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -4,10 +4,10 @@ from typing import Hashable
 
 class LRUCache(dict):
     def __init__(self, limit: int = 42) -> None:
+        super().__init__()
         if not isinstance(limit, int) or limit <= 0:
             raise AttributeError("Limit must be positive int")
         self.limit = limit
-        super().__init__()
 
     def get(self, key: Hashable) -> None:
         if not isinstance(key, ab.Hashable):
