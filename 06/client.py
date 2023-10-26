544ca2438bed83086a42b01c5315f0a7a6c1bcfd
rootinshik <gurianevil@yandex.ru>
2023-10-26 18:38:11 +0300

    Добавлена реализация

---
+++ 06/client.py
@@ -0,0 +1,71 @@
+import threading
+import socket
+import sys
+
+from queue import Queue
+
+
+HOST = "127.0.0.1"
+PORT = 65425
+
+
+class Client:
+    def __init__(self, num_workers: int, file_name: str):
+        self.num_workers = num_workers
+        self.file_name = file_name
+        self.urls_queue = Queue(maxsize=10_000)
+        self.file_worker = FileWorker(self.file_name, self.urls_queue)
+        self.url_workers = [URLWorker(self.urls_queue)
+                            for _ in range(self.num_workers)]
+
+    def start(self):
+        self.file_worker.start()
+        for url_worker in self.url_workers:
+            url_worker.start()
+        self.file_worker.join()
+        for url_worker in self.url_workers:
+            url_worker.join()
+
+
+class FileWorker(threading.Thread):
+    def __init__(self, file_name: str, urls_queue: Queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_name = file_name
+        self.urls_queue = urls_queue
+
+    def run(self):
+        with open(self.file_name, encoding="utf-8") as file:
+            for line in file.readlines():
+                self.urls_queue.put(line.strip())
+        self.urls_queue.put(None)
+
+
+class URLWorker(threading.Thread):
+    def __init__(self, urls_queue: Queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.urls_queue = urls_queue
+
+    def run(self):
+        while True:
+            url = self.urls_queue.get()
+
+            if url is None:
+                self.urls_queue.put(None)
+                break
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+                server.connect((HOST, PORT))
+                server.sendall(url.encode())
+                data = server.recv(1024).decode()
+                print(f"{url}: {data}")
+
+
+def parse_args():
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <num_workers> <file_name>")
+        sys.exit(1)
+    return int(sys.argv[1]), sys.argv[2]
+
+
+if __name__ == "__main__":
+    Client(*parse_args()).start()
---
+++ 06/server.py
@@ -0,0 +1,123 @@
+import threading
+import argparse
+import socket
+import json
+
+from string import ascii_lowercase
+from queue import Queue
+from collections import Counter
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+HOST = "127.0.0.1"
+PORT = 65425
+
+
+class Server:
+    def __init__(self, num_workers: int, k_freq_words: int):
+        self.num_workers = num_workers
+        self.k_freq_words = k_freq_words
+        self.num_processed_urls = 0
+        self.clients_queue = Queue(maxsize=10_000)
+        self.workers = [Worker(self.clients_queue,
+                               self.k_freq_words,
+                               self)
+                        for _ in range(self.num_workers)]
+        self.master = Master(self.clients_queue)
+
+    def start(self):
+        print(f"Starting server with {self.num_workers} "
+              f"workers and {self.k_freq_words} top frequency words.")
+        for worker in self.workers:
+            worker.start()
+        self.master.start()
+        self.master.join()
+        for worker in self.workers:
+            worker.join()
+
+    def __repr__(self):
+        return f"Number of processed URLs: {self.num_processed_urls}"
+
+
+class Master(threading.Thread):
+    def __init__(self, clients_queue: Queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients_queue = clients_queue
+
+    def run(self):
+        self.socket.bind((HOST, PORT))
+        self.socket.listen()
+        while True:
+            client_socket, _ = self.socket.accept()
+            self.clients_queue.put(client_socket)
+
+
+class Worker(threading.Thread):
+    def __init__(self,
+                 clients_queue: Queue,
+                 top_k: int,
+                 server: Server,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.clients_queue = clients_queue
+        self.top_k = top_k
+        self.server = server
+        self.lock = threading.Lock()
+        self.num_urls = 0
+
+    def run(self):
+        while True:
+            client = self.clients_queue.get()
+
+            url = client.recv(1024).decode()
+
+            client.sendall(self.parse_data(url, self.top_k).encode())
+            client.close()
+
+            self.num_urls += 1
+            with self.lock:
+                self.server.num_processed_urls += 1
+
+            print(self.server)
+
+    @staticmethod
+    def get_data(url: str) -> str:
+        with urlopen(url) as response:
+            encoding = response.headers.get_content_charset()
+            return response.read().decode(encoding)
+
+    @staticmethod
+    def parse_data(url: str, top_k: int) -> str:
+        data = (
+            BeautifulSoup(Worker.get_data(url), "html.parser")
+            .get_text()
+            .lower()
+            .split()
+        )
+        words = filter(lambda word: all(symb in ascii_lowercase for symb in word), data)
+        word_count = dict(Counter(words).most_common(top_k))
+        return json.dumps(word_count, ensure_ascii=False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Server part")
+    parser.add_argument("-w",
+                        "--num_workers",
+                        type=int, dest="w",
+                        required=True,
+                        help="provide number of workers")
+    parser.add_argument("-k",
+                        "--top_k_words",
+                        type=int,
+                        dest="k",
+                        required=True,
+                        help="provide top k frequency words")
+    args = parser.parse_args()
+    return args.w, args.k
+
+
+if __name__ == "__main__":
+    Server(*parse_args()).start()
