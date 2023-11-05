bab8d2193467f37710e84b9f6fb503e68d080a73
rootinshik <gurianevil@yandex.ru>
2023-11-05 23:03:28 +0300

    Поправил хинты

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,15 +1,15 @@
 import collections.abc as ab
-from typing import Hashable
+from typing import Hashable, Any
 
 
 class LRUCache(dict):
-    def __init__(self, limit: int = 42) -> None:
+    def __init__(self, limit: int = 42):
         super().__init__()
         if not isinstance(limit, int) or limit <= 0:
             raise AttributeError("Limit must be positive int")
         self.limit = limit
 
-    def get(self, key: Hashable) -> None:
+    def get(self, key: Hashable) -> Any:
         if not isinstance(key, ab.Hashable):
             raise AttributeError("Key must be hashable")
         if key not in self:
@@ -18,7 +18,7 @@ class LRUCache(dict):
         self[key] = val
         return val
 
-    def set(self, key: Hashable, value) -> None:
+    def set(self, key: Hashable, value: Any) -> None:
         if not isinstance(key, ab.Hashable):
             raise AttributeError("Key must be hashable")
         if key in self:
