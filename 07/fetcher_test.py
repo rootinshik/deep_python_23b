8357a2b97d9241c420b0ddbe7e9292d30ae5b739
rootinshik <gurianevil@yandex.ru>
2023-11-05 23:55:36 +0300

    Добавил тест

--- 07/fetcher_test.py
+++ 07/fetcher_test.py
@@ -1,3 +1,4 @@
+import asyncio
 import unittest
 from unittest.mock import patch, mock_open, call
 
@@ -19,6 +20,20 @@ class TestFetcher(unittest.IsolatedAsyncioTestCase):
              for link in TestFetcher.URLS.split("\n")],
             any_order=True
         )
+
+    @patch("fetcher.open", mock_open(read_data=URLS))
+    @patch("fetcher.aiohttp.ClientSession.get")
+    @patch("fetcher.print")
+    async def test_num_tasks(self, _, __):
+        fetcher = Fetcher(10, "/dev/null")
+        loop = asyncio.get_event_loop()
+        await fetcher.batch_fetch()
+        self.assertEqual(11, len(asyncio.all_tasks(loop)))
+
+        fetcher = Fetcher(5, "/dev/null")
+        loop = asyncio.get_event_loop()
+        await fetcher.batch_fetch()
+        self.assertEqual(6, len(asyncio.all_tasks(loop)))
 
 
 if __name__ == "__main__":
